d6218c02586e0b01809cf1e7cee292e552cbd453
Peter Wardell <peterwardell86@gmail.com>
2024-04-20 11:04:33 -0400

    More time fixes

--- app/TokenUtil.py
+++ app/TokenUtil.py
@@ -1,8 +1,8 @@
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 import jwt
 
 def get_token(username, timeout, secret_key):
-    return jwt.encode({'username': username, 'exp': datetime.now(timezone.utc) + datetime.timedelta(seconds=timeout)}, secret_key)
+    return jwt.encode({'username': username, 'exp': datetime.now(timezone.utc) + timedelta(seconds=timeout)}, secret_key)
     
 def is_token_expired(token_data):
     return token_data['exp'] > datetime.now(timezone.utc).timestamp()
